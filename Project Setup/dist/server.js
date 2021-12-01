3ad4679a50eefbc3ca111d37d66bd835f3d31f01
Hamza Zaheer <hamzazaheer721@gmail.com>
2021-12-01 12:28:13 +0500

    !feat: nullChecks explained

--- Project Setup/dist/server.js
+++ Project Setup/dist/server.js
@@ -1,6 +1,10 @@
 "use strict";
 console.log("App is running!");
 let button = document.querySelector("button");
+// null error exists because typescript can't dive into your html and check if your button really does exist
+// so to make sure you don't use that certainly doesn't exist, typescript asks you to tell it that yes it sure isn't null with !
+// or you can disable the strict mode
+// or you can disable strickNullChecks 
 button.addEventListener("click", (e) => {
     e.stopPropagation();
     e.preventDefault();
