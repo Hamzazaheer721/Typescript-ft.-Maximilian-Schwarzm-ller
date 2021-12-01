99d06bf2f0784ecc0e0fca07b75c960aa4772034
Hamza Zaheer <hamzazaheer721@gmail.com>
2021-12-01 19:00:24 +0500

    !feat: readonly descripton added

--- 4_OOP/iv_shortands.ts
+++ 4_OOP/iv_shortands.ts
@@ -1,7 +1,10 @@
 class Person {
   // Instead of defining class variables everytime, you can simply use access modifier property before params in constructor
   // class varibles will be initialized with exactly the same name and value
-  constructor(public name: string , private age: number = 24){}
+  // I have added readonly property before the name of the string
+  // You can never change the value of the name variable once it has been set
+  // without shortand I would have defined it like readonly name: string
+  constructor(public readonly name: string , private age: number = 24){}
 
   get getInformation(){
     console.log(`Name: ${this.name}`)
