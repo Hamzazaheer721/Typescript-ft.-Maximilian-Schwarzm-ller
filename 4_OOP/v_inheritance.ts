02402508d505eba81166b538b759807c1c6db1bb
Hamza Zaheer <hamzazaheer721@gmail.com>
2021-12-02 10:14:43 +0500

    !feat: inheritance and overriding the methods in child classes have been implemented

---
+++ 4_OOP/v_inheritance.ts
@@ -0,0 +1,42 @@
+class Department {
+  constructor(public employees: string[]) {}
+
+  get getDepartmentName() {
+    return this.employees
+  }
+
+  set addDepartments(_emp: string) {
+    this.employees.push(_emp)
+  }
+}
+
+const department = new Department(["Ali Ahmad"])
+
+class ITDepartment extends Department {}
+
+const itDept = new ITDepartment(["Hamza"]) // it now is pointing to the constructore of department class
+
+////////////////////////////////
+/////////// MORE //////////////
+//////////////////////////////
+
+class AccountingDepartment extends Department {
+  // it has access to all the methods and variables of its parent class now except the private methods and private properties of parents
+  // to make private properties accessible in child classes, you will have to make them protected 
+  constructor(_emp: string[], private reports: string[]) {
+    super(_emp)
+  }
+
+  printReports(this: AccountingDepartment) {
+    console.log(`Employees: `, this.employees)
+    console.log(`Reports: `, this.reports)
+  }
+}
+
+const accDept = new AccountingDepartment(
+  ["Zubair"],
+  ["Exam Reports", "Salary Reports"]
+)
+
+console.log(`Employees: `, accDept.employees)
+accDept.printReports()
---
+++ 4_OOP/vi_overridingProperties.ts
@@ -0,0 +1,67 @@
+class Department {
+  constructor(protected id: number) {}
+}
+
+class ITDepartment extends Department {
+  protected lastEmployee?: string
+  constructor(id: number, protected employees: string[]) {
+    super(id)
+    this.lastEmployee = employees[employees.length - 1]
+  }
+
+  get getEmployees() {
+    return this.employees
+  }
+
+  get getLastEmployee() {
+    const { lastEmployee } = this
+    if (lastEmployee) {
+      return lastEmployee
+    }
+  }
+
+  addEmployee(_emp: string) {
+    this.employees.push(_emp)
+  }
+}
+
+class AIDepartment extends ITDepartment {
+  constructor(id: number, employees: string[], private location: string) {
+    super(id, employees)
+  }
+
+  //Overriding addEmployee
+  addEmployee(_emp: string) {
+    if (_emp === "Hamza") return
+    this.employees.push(_emp)
+    const { employees } = this
+    this!.lastEmployee = employees[employees.length - 1]
+  }
+
+  set setLocation(_loc: string) {
+    if(!_loc){
+      throw new Error("Please enter a Location !!")
+    }
+    this.location = _loc
+  }
+
+  get getLocation () {
+    return this.location
+  }
+}
+
+const aiDept = new AIDepartment(42, ["Saad", "Zubair"], "Gujranwala")
+
+aiDept.addEmployee("Hamza") // rejected
+console.log(aiDept.getEmployees) // ["Saad", "Zubair"]
+console.log(aiDept.getLastEmployee) // Zubair
+aiDept.addEmployee("Arbaz")
+console.log(aiDept.getLastEmployee) //Arbaz //Note: we access getters just as property not as method
+
+aiDept.setLocation = "Lahore"
+console.log(aiDept.getLocation) // Lahore
+
+// console.log(aiDept.setLocation("Islamabad")) // we can't access it like method, we have to access setters, getters as property
+aiDept.setLocation = "" // Please enter a Location !!!
+
+
