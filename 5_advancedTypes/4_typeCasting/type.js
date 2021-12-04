13bd59b802d155392689faf8c732c6c49a16d4f2
Hamza Zaheer <hamzazaheer721@gmail.com>
2021-12-04 23:06:11 +0500

    !feat: typeCasting concept completed

--- 5_advancedTypes/4_typeCasting/type.js
+++ 5_advancedTypes/4_typeCasting/type.js
@@ -1,8 +1,14 @@
 "use strict";
 // typescript knows the specific type of element when you get an element by querySelector
 //Typescript knows that the type of paragraph variable is HTMLParagraphElement | null
-// const paragraph = document.querySelector("p") 
+// const paragraph = document.querySelector("p")
 // Step 2
 // In following case typescript doesn't know the exact type of paragraph, it only knows it is HTMLElement | null
-const paragraph = document.getElementById("greetings_paragraph");
+// We can use angle brackets <> to typecast or we could also use "as" keyword to typeCast
+const paragraph = (document.getElementById("greetings_paragraph"));
 // why its type rn is null ? thats because typescript doesn't know what exists in your html file
+paragraph.innerHTML = "Hello!";
+const input = document.getElementById("input__field");
+if (input) {
+    input.value = "Custom Value is being added ! ";
+}
--- 5_advancedTypes/4_typeCasting/type.ts
+++ 5_advancedTypes/4_typeCasting/type.ts
@@ -1,10 +1,21 @@
 // typescript knows the specific type of element when you get an element by querySelector
 //Typescript knows that the type of paragraph variable is HTMLParagraphElement | null
-// const paragraph = document.querySelector("p") 
+// const paragraph = document.querySelector("p")
 
 // Step 2
 
 // In following case typescript doesn't know the exact type of paragraph, it only knows it is HTMLElement | null
-const paragraph = document.getElementById("greetings_paragraph")
+// We can use angle brackets <> to typecast or we could also use "as" keyword to typeCast
+
+const paragraph = <HTMLParagraphElement>(
+  document.getElementById("greetings_paragraph")
+)
 
 // why its type rn is null ? thats because typescript doesn't know what exists in your html file
+
+paragraph.innerHTML = "Hello!"
+
+const input = document.getElementById("input__field")
+if (input) {
+  (input as HTMLInputElement).value = "Custom Value is being added ! "
+}
