fb1167462d16e7e8035370e0bbcfd8210a6c09d2
Hamza Zaheer <hamzazaheer721@gmail.com>
2021-12-08 12:40:15 +0500

    !feat: Shallow copy and deep copy explained

---
+++ helper Functions/index.ts
@@ -0,0 +1,42 @@
+
+////////////////////////////////////////////////
+////////////// Ways to copy Object ////////////
+//////////////////////////////////////////////
+
+const _obj : {name: string, age: number, class: string} = {
+  age: 21,
+  name: "hamza",
+  class: "12th grade"
+}
+
+// We can do the following using 
+// Spread
+// Object.assign
+// JSON
+
+// Method 1
+let p1 = {
+  ..._obj
+}
+
+// Method 2 
+p1 = Object.assign({}, _obj)
+
+// Method 3
+p1 = JSON.parse(JSON.stringify(_obj))
+
+
+
+
+/////////////////////// Shallow Copy Method /////////////////////
+
+const shallowCopy = <T extends object>(obj: T): T => {
+    return {... obj}
+}
+
+
+/////////////////// Deeep Copy Method //////////////////////////
+
+const deepCopy = <T extends object>(obj: T) => {
+  return JSON.parse(JSON.stringify(obj))
+}
