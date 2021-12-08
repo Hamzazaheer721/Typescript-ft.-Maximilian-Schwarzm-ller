14fdfdd20d799d8c28133caa891d3be0cd83cbb6
Hamza Zaheer <hamzazaheer721@gmail.com>
2021-12-08 17:35:52 +0500

    !feat: merge arrays generic method has been made

--- helper Functions/index.ts
+++ helper Functions/index.ts
@@ -36,17 +36,42 @@ const deepCopy = <T extends object>(obj: T): T => {
   return JSON.parse(JSON.stringify(obj))
 }
 
-///////////////// Better Deep Copy Method /////////////////////
+////////////////////////////////////////////////////
+/////////// Better Deep Copy Method ///////////////
+//////////////////////////////////////////////////
+
 const _deepClone = <T extends object>(obj: T): T => {
   const _copy: unknown = {}
   for (let key in obj) {
     let _obj = obj[key] as unknown;
-    if ((typeof _obj === "object" && _obj !== null) {
+    if (typeof _obj === "object" && _obj !== null) {
       _copy[key] = _deepClone(_obj)
     }
     _copy[key] = obj[key];
   }
   return _copy as T
 }
+
 // this method will return Deep Copy of the object of any kind that we will pass in.
 console.log(_deepClone({ name: "Hamza", age: 21, e: { name: "ad" } }))
+
+/////////////////////////////////////////
+///////////// MERGE THE ARAY //////////
+///////////////////////////////////////
+
+const mergeArrays = <T>(...arrays: Array<T>[]): Array<T> => {
+  const _arr: T[] = []
+  for(let array of arrays){
+    _arr.push(...array)
+  }
+  return _arr
+}
+
+const _array1 = [1,2,3,4]
+const _array2 = [5,6,7,8]
+const _array3 = [9,10]
+const _array4 = [11,12,13]
+
+const _mergedArrays = mergeArrays<number>(_array1, _array2, _array3, _array4)
+
+console.log(_mergedArrays)
