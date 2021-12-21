e40678ce0771fc8c7440620206d1b0c59c0198e3
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-12-22 01:14:05 +0500

    !feat: string to number enum is being implemnted

--- Techniques/switchAlternate.ts
+++ Techniques/switchAlternate.ts
@@ -25,3 +25,42 @@ let filterMethod = (status: string): IActionString => {
 console.log(filterMethod('loading')) // LOADING
 
 // Now lets see another trick
+enum IActionNumber {
+  LOADING,
+  ERROR,
+  SUCCESS
+}
+
+// { 0: 'LOADING',
+//   1: 'ERROR',
+//   2: 'SUCCESS',
+//   LOADING: 0,
+//   ERROR: 1,
+//   SUCCESS: 2 }
+
+console.log(Object.values(IActionNumber)) // [ 'LOADING', 'ERROR', 'SUCCESS', 0, 1, 2 ]
+console.log(Object.keys(IActionNumber)) // [ '0', '1', '2', 'LOADING', 'ERROR', 'SUCCESS' ]
+console.log(IActionNumber)
+
+const _filterMethod = (status: number | string) => {
+  if (typeof status === 'string') {
+    status = status.toUpperCase().trim()
+  }
+  console.log(status)
+  let _index = Object.values(IActionNumber).indexOf(status)
+  console.log(_index)
+  if (_index === -1) return -1 // not found
+  switch (_index) {
+    case IActionNumber.LOADING:
+      return status
+    case IActionNumber.ERROR:
+      return status
+    case IActionNumber.SUCCESS:
+      return status
+    default:
+      return -1
+  }
+}
+
+console.log(IActionNumber.LOADING) // 0
+console.log(_filterMethod('success')) // SUCCESS
