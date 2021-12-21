8a4d67ef2ce778ac3c7a7e1b1152c7ed5be79c5d
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-12-21 23:11:15 +0500

    !feat: formatted code

--- 2_basics/xii_destructuring.ts
+++ 2_basics/xii_destructuring.ts
@@ -1,57 +1,61 @@
-const hobbies = ["Hiking", "Coding", "Gaming", "Cycling"]
+const hobbies = ['Hiking', 'Coding', 'Gaming', 'Cycling'];
 
 ///////////////////////////////////////////
 ///////// Array Destructuring ////////////
 /////////////////////////////////////////
 
-// Method # 1
-const firstHobby = hobbies[0]
-console.log(firstHobby) // Hiking
+const firstHobby = hobbies[0];
+console.log(firstHobby); // Hiking
 
-// Method # 2
-const [,,,Cycling] = hobbies
-console.log(Cycling) //Cycling 
-
-// Better way 
-// Since array is also an iteratable object so we can destruct it with its indices and name those indices like we usually do in object
-const {3: _Cycling} = hobbies 
-console.log(_Cycling) //Cycling 
- 
 //Destructering the String itself
-let [firstLetter] = firstHobby
-console.log(firstLetter) // H
+let [firstLetter] = firstHobby;
+console.log(firstLetter); // H
 
-//Destructuring the Array now
-const [_first, _second, ...rest] = hobbies
-console.log("First Method: ", _first, _second) //Hiking Coding
-console.log("Second Method: ", ...hobbies) // Hiking Coding
+//////////////////////////////////////////////////
+//////////////// Destructuring //////////////////
+////////////////////////////////////////////////
+
+// Method # 1
+const [_first, _second, ...rest] = hobbies;
+console.log('First Method: ', _first, _second); //Hiking Coding
+console.log('Second Method: ', ...hobbies); // Hiking Coding
 
 // ...rest is wrapping the rest of the hobbies values into array, since there are no elements left so it will be [] in this case
 //  ... operator in parameters is called Rest Parameter and it will turn the arguments into array
 // however, when used usually the spread operator spreads the array back into array values one by one
-console.log(rest) // []
+console.log(rest); // []
+
+// Method # 2
+const [, , , Cycling] = hobbies;
+console.log(Cycling); //Cycling
+
+// Better way
+// Since array is also an iteratable object so we can destruct it with its indices and name those indices like we usually do in object
+const { 2: valAtSecondIndex, 3: _Cycling } = hobbies;
+console.log(_Cycling); //Cycling
+console.log(valAtSecondIndex); // Gaming
 
 ////////////////////////////////
 //////// EXTRA ////////////////
 //////////////////////////////
 
-const rainForests = ["Amazon", "Borneo", "Cerrado", "Congo"]
+const rainForests = ['Amazon', 'Borneo', 'Cerrado', 'Congo'];
 // it will slice the array from 0 till 2 index but won't include value at 2nd index
 
-console.log(rainForests.splice(0, 2)) // ["Amazon", "Borneo"]
-console.log(rainForests) // ["Cerrado, "Congo"]
+console.log(rainForests.splice(0, 2)); // ["Amazon", "Borneo"]
+console.log(rainForests); // ["Cerrado, "Congo"]
 
 // in case of slice, we are return shallow copy of the portion of an array to new array object selected from start to end(end not included)
 // hence original array is not modified
-const _rainForests = ["Amazon", "Borneo", "Cerrado", "Congo"]
-console.log(_rainForests.slice(0, 1)) // ["Amazon"]
-console.log(_rainForests.slice(0, 1)) // ["Amazon"]
-console.log(_rainForests.slice(1)) // [ 'Borneo', 'Cerrado', 'Congo' ]
-console.log(_rainForests.slice(1, 2)) // ['Borneo']
-console.log(_rainForests.slice(2, 2)) // []
-console.log(_rainForests.slice(2, 3)) // ["Cerrado"]
-console.log(_rainForests.slice(0, 2)) // ["Amazon", "Borneo"]
-console.log(_rainForests) // ["Amazon", "Borneo","Cerrado, "Congo"]
+const _rainForests = ['Amazon', 'Borneo', 'Cerrado', 'Congo'];
+console.log(_rainForests.slice(0, 1)); // ["Amazon"]
+console.log(_rainForests.slice(0, 1)); // ["Amazon"]
+console.log(_rainForests.slice(1)); // [ 'Borneo', 'Cerrado', 'Congo' ]
+console.log(_rainForests.slice(1, 2)); // ['Borneo']
+console.log(_rainForests.slice(2, 2)); // []
+console.log(_rainForests.slice(2, 3)); // ["Cerrado"]
+console.log(_rainForests.slice(0, 2)); // ["Amazon", "Borneo"]
+console.log(_rainForests); // ["Amazon", "Borneo","Cerrado, "Congo"]
 
 //////////////////////////////////////////////////////////////////
 ////// Difference b/w spread Operator & Rest Parameter //////////
