ecbcabfe39c16f1eb1e7871443621a24a92f1fad
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-01-05 20:35:32 +0500

    !feat: array operations with splice practised

--- helper Functions/splice_slice.ts
+++ helper Functions/splice_slice.ts
@@ -19,3 +19,29 @@ console.log(_rainForests.slice(2, 2)) // []
 console.log(_rainForests.slice(2, 3)) // ["Cerrado"]
 console.log(_rainForests.slice(0, 2)) // ["Amazon", "Borneo"]
 console.log(_rainForests) // ["Amazon", "Borneo","Cerrado, "Congo"]
+
+const _arr = [1, 2, 3, 4, 5, 6]
+let _res: Partial<typeof _arr> = []
+
+const addFunc = (index, arr, val) => {
+  if (index > arr.length) return arr
+  arr.splice(index, 0, val)
+  // we said from index to onwards but its 0 so we won't delete any
+  return arr
+}
+
+const removeFunc = (val, arr) => {
+  if (arr.indexOf(val) === -1) return _arr
+  arr.splice(arr.indexOf(val), 1)
+  return arr
+}
+
+const replaceFunc = (index, arr, val) => {
+  if (index > arr.length) return arr
+  arr.splice(index, 1, val)
+  return arr
+}
+
+console.log('ADD', addFunc(2, _arr, 33))
+console.log('REMOVE', removeFunc(33, _arr))
+console.log('REPLACE', replaceFunc(5, _arr, 66))
