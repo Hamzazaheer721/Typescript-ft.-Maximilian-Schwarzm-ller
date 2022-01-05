62d344a087e71556d5df261c9d7b06152ddf662d
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-01-05 20:17:16 +0500

    !feat: replace generic function has been added wth splice

--- 6_generics/practice/1_interfaces.ts
+++ 6_generics/practice/1_interfaces.ts
@@ -12,11 +12,31 @@ const addFunc: IAddFuncProps = (index, val, array) => {
   return array
 }
 
-const _arr: Array<string> = ["Hamza", "Zaheer", "Arbaz", "Tahir"]
+const _arr: Array<string> = ['Hamza', 'Zaheer', 'Arbaz', 'Tahir']
 
-let _udpatedArr = addFunc(2, "Azam", _arr)
+let _udpatedArr = addFunc(2, 'Azam', _arr)
 
 console.log(_udpatedArr) // [ 'Hamza', 'Zaheer', 'Azam', 'Arbaz', 'Tahir' ]
+
+///////////////////////////////////////////////////////////
+/////// Function to replace element at specific index ////////
+/////////////////////////////////////////////////////////
+
+interface IReplaceFuncProps {
+  <T>(a: number, b: T, c: T[]): T[]
+}
+
+const replaceFunc: IReplaceFuncProps = (index, val, array) => {
+  if (index > array.length) return array
+  array.splice(index, 1, val)
+  return array
+}
+
+const _arrR: Array<string> = ['Hamza', 'Zaheer', 'Arbaz', 'Tahir']
+
+let _udpatedArrR = replaceFunc(2, 'Azam', _arrR)
+
+console.log(_udpatedArrR) // [ 'Hamza', 'Zaheer', 'Azam', 'Tahir' ]
 
 //////////////////////////////////////////////////////////////////////////////
 ////////// Non generic Function to delete element at specific Index /////////
@@ -34,38 +54,38 @@ const removeFunc: removeFuncType = (val, array) => {
   return array
 }
 
-const arr_: Array<string> = ["Hamza", "Zaheer", "Arbaz", "Tahir"]
+const arr_: Array<string> = ['Hamza', 'Zaheer', 'Arbaz', 'Tahir']
 
-let _res = removeFunc("Hamza", arr_)
+let _res = removeFunc('Hamza', arr_)
 
 console.log(_res) // [ 'Zaheer', 'Arbaz', 'Tahir' ]
 
-let res_ = removeFunc<boolean>(false, [true, false , false])
+let res_ = removeFunc<boolean>(false, [true, false, false])
 console.log(res_) // [true, false] //deletes the false that it finds in array
 
 ///////////////////////////////////////////////////////////
 /////////////// Check if Object is Empty /////////////////
 /////////////////////////////////////////////////////////
 
-const isObjectEmpty = <T extends object> (_obj: T): boolean => {
-  const _res = true;
-  for (let key in _obj){
-    if(_obj.hasOwnProperty(key)) return false
+const isObjectEmpty = <T extends object>(_obj: T): boolean => {
+  const _res = true
+  for (let key in _obj) {
+    if (_obj.hasOwnProperty(key)) return false
   }
-  return _res;
+  return _res
 }
 
 // console.log(isObjectEmpty(23)) // error! number is not assignable to object
-console.log(isObjectEmpty({name: "hamza"}))
+console.log(isObjectEmpty({ name: 'hamza' }))
 
-console.log(Object.keys({name: "Hamza"})) // ['name'] // returns array of keys in object
+console.log(Object.keys({ name: 'Hamza' })) // ['name'] // returns array of keys in object
 
 ///////////////////////////////////////////////////////////
 ///////////// Get first Element in Object ////////////////
 /////////////////////////////////////////////////////////
 
-const getFirstKey = <T extends object>(_obj: T): string=> {
-  let key: string;
+const getFirstKey = <T extends object>(_obj: T): string => {
+  let key: string
   for (let _key in _obj) {
     key = _key
     break
@@ -73,4 +93,4 @@ const getFirstKey = <T extends object>(_obj: T): string=> {
   return key
 }
 
-console.log(getFirstKey({name: "Azam", age: 32})) // name
+console.log(getFirstKey({ name: 'Azam', age: 32 })) // name
