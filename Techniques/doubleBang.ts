672203ec8adbd26ecbf513519626c9083a9e2615
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-01-09 03:46:32 +0500

    !feat(double-bang): implemented double bang concept

---
+++ Techniques/doubleBang.ts
@@ -0,0 +1,29 @@
+let str = "Dani"
+let flag = true
+let number_1 = 1
+let number_2 = 0
+let array = []
+let array_2 = [2, 3]
+let obj_ = {}
+let obj__ = { name: "Hamza" }
+
+console.log("str: ", str)
+console.log("!!str: ", !!str)
+
+console.log("!!flag ", !!flag)
+
+console.log(`number: ${!!number_2}`)
+
+console.log(`!!array.length: ${!!array.length}`)
+console.log(`array.length: ${array.length}`)
+
+console.log(`!!array_2.length: ${!!array_2.length}`)
+console.log(`array_2.length: ${array_2.length}`)
+
+console.log(`obj_: ${obj_}`)
+console.log(`!!obj_: ${!!obj_}`)
+
+console.log(`obj__: ${obj__}`)
+console.log(`!!obj__: ${!!obj__}`)
+
+console.log(`number: ${!!number_2}`)
