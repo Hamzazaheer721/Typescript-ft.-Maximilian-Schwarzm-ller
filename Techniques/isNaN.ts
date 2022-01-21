dc8c1ae7cdf2ea22785536f4f6471fe234b4b324
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-01-22 02:52:18 +0500

    !feat(isNan): isNan understood

---
+++ Techniques/isNaN.ts
@@ -0,0 +1,12 @@
+console.log(`NaN===NaN: ${NaN === NaN}`)
+console.log(`NaN!==NaN: ${NaN !== NaN}`)
+
+const isNan = <T>(val: T): boolean => {
+  let n = +val
+  console.log('value after typecasting :', n)
+  console.log(`n===n: ${n} === ${n} =>  ${n === n}`)
+  console.log(`n!==n: ${n} === ${n} => ${n !== n}`)
+  return n !== n
+}
+
+console.log(isNan('92'))
