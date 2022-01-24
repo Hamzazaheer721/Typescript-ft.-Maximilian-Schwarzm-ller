e6f7a42c5c6ac186ae575a25dadde9440a374fa0
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-01-24 22:10:49 +0500

    !feat(techniques): flattenArray revised

---
+++ Techniques/flattenArray.ts
@@ -0,0 +1,18 @@
+interface IFlattenArray {
+  (a: any, b: number): any
+}
+
+const flattenArray_: IFlattenArray = (arr, depth) => {
+  return depth > 0
+    ? arr.reduce(
+        (acc, curr) =>
+          acc.concat(
+            Array.isArray(curr) ? flattenArray_(curr, depth - 1) : curr
+          ),
+        []
+      )
+    : arr.slice()
+}
+
+const array_ = [[[[[[[[[23, 3]], 3, 2]]]]]], 32, 53, 12]
+console.log(flattenArray_(array_, Infinity))
