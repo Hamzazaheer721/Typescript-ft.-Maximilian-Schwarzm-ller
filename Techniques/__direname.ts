f40a759faa03a165d67b62dd3eef30dd5b2c55a1
adrpage <cctoadrian@gmail.com>
2022-02-08 15:56:22 -0600

    !feat: test

---
+++ Techniques/__direname.ts
@@ -0,0 +1,8 @@
+const path = require('path')
+
+console.log(`Direname: ${__dirname}`)
+console.log(`process.cwd(): ${process.cwd()}`)
+console.log(`process.resolve(./): ${path.resolve('./')}`)
+console.log(`filename:  ${__filename}`)
+
+/*Direname: D:\Practise\Typescript-ft.-Maximilian-Schwarzm-ller\Techniques*/
