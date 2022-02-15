2579f6e15330e7c784ce88cec33cb5c84ef87f3a
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-02-15 05:38:23 +0500

    !feat(hoisting): revision

---
+++ Revision/Hoisting.ts
@@ -0,0 +1,9 @@
+var number = 20
+const func_ = () => {
+  console.log(number) // undefined
+  var number = 10
+  console.log(number) // 10
+}
+
+console.log(number) // 20
+console.log(func_()) // undefined
