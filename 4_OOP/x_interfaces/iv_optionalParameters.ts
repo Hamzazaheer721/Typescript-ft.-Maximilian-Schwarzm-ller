657e6121febb2a3029bf7e0123fea5d5ffcfc175
Hamza Zaheer <hamzazaheer721@gmail.com>
2022-02-25 14:03:21 -0600

    !feat(objComparison): Revision on interfaces is being done

--- 4_OOP/x_interfaces/iv_optionalParameters.ts
+++ 4_OOP/x_interfaces/iv_optionalParameters.ts
@@ -4,16 +4,16 @@ interface IPerson {
 }
 
 class Person implements IPerson {
-  name?: string;
-  constructor(_name?: string){
-    if(_name){
+  name?: string
+  constructor(_name?: string) {
+    if (_name) {
       this.name = _name
     }
   }
-  greet(_val: string){
-    console.log(`Hello there : `, _val )
+  greet(_val: string) {
+    console.log(`Hello there : `, _val)
   }
 }
 
-const person = new Person("Hamza")
+const person = new Person('Hamza')
 console.log(person.name) // Hamza
---
+++ Revision/objComparison.ts
@@ -0,0 +1,46 @@
+enum ICountryActions {
+  PAKISTAN,
+  INDIA,
+  SOUTHASIA
+}
+
+type CityPakistan = 'karachi' | 'lahore' | 'peshawar' | 'quetta'
+type CityIndia = 'mumbai' | 'delhi' | 'haryana' | 'hyderabad'
+type CitySouthAsia = CityIndia & CityPakistan
+
+interface IAddressPakistan {
+  country: ICountryActions.PAKISTAN
+  city: CityPakistan
+  street: string
+  state: string
+}
+
+interface IAddressIndia {
+  country: ICountryActions.INDIA
+  city: CityIndia
+  street: string
+  state: string
+}
+
+interface IAddressSouthAsia {
+  country: ICountryActions.SOUTHASIA
+  city: CitySouthAsia
+  street: string
+  state: string
+}
+
+interface IPerson {
+  name: string
+  age: number
+}
+
+interface ISouthAsianPerson extends IPerson, IAddressSouthAsia {
+  name: string
+  age: number
+  country: ICountryActions.SOUTHASIA
+  city: CitySouthAsia
+  street: string
+  state: string
+}
+
+interface Developer {}
