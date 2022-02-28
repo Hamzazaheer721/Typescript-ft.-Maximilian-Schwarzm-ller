74d1688605dff2df489d54ff9a6b4b79af451cbc
Hamza Zaheer <hamzazaheer721@gmail.com>
2022-02-28 13:40:39 -0600

    !feat(ShallowCopy): Shallow Copying obj

--- 2_basics/xiii_destructering.ts
+++ 2_basics/xiii_destructering.ts
@@ -2,12 +2,27 @@
 // Array is an ordered list while in object, order is not always guranteed
 
 const person = {
-  name: "hamza",
+  name: 'hamza',
   age: 42
 }
 
-const _obj = {name: person.name, ...person}
+const _obj = { name: person.name, ...person }
 
-console.log(_obj)  // {name: "hamza", age: 42}
+console.log(_obj) // {name: "hamza", age: 42}
 
-const {age, name: _objName} = _obj;
+const { age, name: _objName } = _obj
+
+// Shallow Copying the object
+
+/* Method # 1 */
+const { ...clone } = _obj
+
+/* Method # 2 */
+const clone_ = Object.assign({}, _obj)
+
+/* Method # 3 */
+const clone__ = { ..._obj }
+
+console.log(clone) // {name: "hamza", age: 42}
+console.log(clone_) // {name: "hamza", age: 42}
+console.log(clone__) // {name: "hamza", age: 42}
