5a278dbf8b239c6c87317707460914dc0e03e154
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-03-28 01:57:00 -0400

    feat (Date): Date format simple conversion

---
+++ Techniques/DateToYYYY-MM-DD.ts
@@ -0,0 +1,10 @@
+
+const date = new Date();
+
+console.log(date); // 2022-03-28T05:54:59.507Z
+
+console.log(date.toString()); // Mon Mar 28 2022 05:54:59 GMT+0000 (GMT)
+
+console.log(date.toISOString()); // 2022-03-28T05:54:59.507Z
+
+console.log(date.toISOString().split("T")[0]); // 2022-03-28
