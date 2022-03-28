a10480d17327ae6cf1fd2588c04944bc8b62edcd
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-03-28 09:36:54 -0400

    !feat (Object): Description has been updated

--- Techniques/isFrozen.ts
+++ Techniques/isFrozen.ts
@@ -1,5 +1,6 @@
 // Object.isFrozen() is used to determine if object is frozen
-
+// An object is frozen if and only if it is not extensible, all its properties are non-configurable, 
+// and all its data properties (that is, properties which are not accessor properties with getter or setter components) are non-writable.
 
 const empty_obj = {}
 console.log(Object.isFrozen(empty_obj)) // false
