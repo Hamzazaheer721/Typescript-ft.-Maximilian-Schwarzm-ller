7303671b05b32570add58cddbf4bf13e851150a5
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-03-28 12:19:05 -0400

    !feat (Pyramid): made Pyramid

---
+++ Data Structures/pyramids.ts
@@ -0,0 +1,31 @@
+// *
+// ***
+// *****
+// *******
+// *********
+// ***********
+// *************
+// ***************
+// *************
+// ***********
+// *********
+// *******
+// *****
+// ***
+// *
+
+
+const makePyramid = (length = 12) => {
+    const half = Math.ceil(length/2)
+    let n = 0;
+    for (let i = 0; i <= half; i++){
+        n = 2 * i + 1
+        console.log("*".repeat(n))
+    }
+    for(let i = half - 1; i >=0; i-- ){
+        n = 2 * i + 1
+        console.log("*".repeat(n))
+    }
+}
+
+makePyramid(14)
