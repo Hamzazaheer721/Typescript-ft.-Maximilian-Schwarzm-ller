5de802a680587aac4e754fbdd63f9acc24f4b5d8
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-03-28 05:51:55 -0400

    !feat (Objects immutability): immutablity article link attached

---
+++ Techniques/ObjectImmutablity.ts
@@ -0,0 +1,3 @@
+
+// Object Immutablity
+// https://blog.logrocket.com/javascript-object-immutability-object-freeze-vs-object-seal/#:~:text=seal()-,With%20Object.,ones%2C%20you%20can%20make%20changes.
