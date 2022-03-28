c5873dabb511188e6529248e61803b8a78b429c9
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-03-28 06:57:57 -0400

    !feat (Objects): Object.seal() vs Object.freeze()

---
+++ Techniques/ObjectSealVsObjectFreeze.ts
@@ -0,0 +1,45 @@
+// With Object.freeze(), new changes have no effect on the frozen object. 
+// However, the seal() method allows modifying existing properties. 
+// This means that while you cannot add new properties or remove existing ones, you can make changes.
+
+let obj = {
+    name: "Hamza",
+    age: 25
+}
+
+Object.freeze(obj)
+obj.name = "Zaheer"
+obj.city = "Islamabad"
+
+console.log(obj) // { name: 'Hamza', age: 25 }
+
+/* { name:
+  { 
+    value: 'Hamza',
+    writable: false,
+    enumerable: true,
+    configurable: false 
+  } 
+} */
+
+let obj_2 = {
+    name: "Ali"
+}
+
+Object.seal(obj_2)
+obj_2.name = "Hamza"
+obj_2.age = 25;
+
+console.log(obj_2) // { name : "Hamza" }
+
+console.log(Object.getOwnPropertyDescriptors(obj_2)) 
+/* { name:
+  { 
+    value: 'Hamza',
+    writable: true,
+    enumerable: true,
+    configurable: false 
+  } 
+} */
+
+
