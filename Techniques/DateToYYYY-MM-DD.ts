b994be3e5d345024d1438799660fb38fb59523ca
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-03-28 04:10:00 -0400

    !feat (Date): Date format changed

--- Techniques/DateToYYYY-MM-DD.ts
+++ Techniques/DateToYYYY-MM-DD.ts
@@ -1,4 +1,3 @@
-
 const date = new Date();
 
 console.log(date); // 2022-03-28T05:54:59.507Z
@@ -7,4 +6,6 @@ console.log(date.toString()); // Mon Mar 28 2022 05:54:59 GMT+0000 (GMT)
 
 console.log(date.toISOString()); // 2022-03-28T05:54:59.507Z
 
-console.log(date.toISOString().split("T")[0]); // 2022-03-28
+console.log(date.toISOString().split("T")[0]) // // 2022-03-28
+
+console.log(date.toISOString().split("T")[0].split("-").reverse().join("-")); // 28-03-2022
