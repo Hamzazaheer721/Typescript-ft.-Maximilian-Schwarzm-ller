815364facf78a50d26113e6c4b7fe8de9cfa3db7
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-05-06 22:36:51 +0500

    !feat(Techniques): selecting random element

---
+++ Techniques/pickingRandomArrayElement.ts
@@ -0,0 +1,8 @@
+const array: string[] = ["Apples", "Mangoes", "Pears"]
+
+const elem: string = Math.floor(Math.random() * array.length);
+
+console.info(array[elem]) // Pears
+console.info(array[elem]) // Apples
+console.info(array[elem]) // Pears
+console.info(array[elem]) // Mangoes
