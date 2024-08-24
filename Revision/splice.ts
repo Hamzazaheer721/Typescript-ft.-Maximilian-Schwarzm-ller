76b19902eb72fe49884f6693a1a93852085c2fc4
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2024-08-24 11:09:28 +0500

    feature-revision: splice revisited

--- Revision/splice.ts
+++ Revision/splice.ts
@@ -27,3 +27,36 @@ const addElement = <T>(array: T[], valueToAdd: T, index: number): T[] => {
 console.log(removeElement(arr1, 'Haseem'))
 console.log(replaceElement(arr2, 'Haseem', 'Haseeb'))
 console.log(addElement(arr3, 'Ahmad', 2))
+
+
+
+
+// const a = 'DDUUDDUDUUUD'.trim(); 
+
+// return value of match is array of string if atleast one match exists
+// if the match doesn't exist then it will return null
+const matches = a.match(/DDU/g); 
+const count = matches ? matches.length : 0; 
+
+let position = a.indexOf('DDU'); // 0
+// return value of indexOf is number 
+// in falsy case, it will return -1
+let count_two = 0;
+
+while (position !== -1) {
+    count_two++;
+    position = a.indexOf('DDU', position + 1);
+}
+
+// Revision: 24/8/2024
+const months = ['Jan', 'March', 'April', 'June'];
+
+// Delete
+const elem = "March";
+months.splice(months.indexOf(elem), 1)
+
+// Add
+months.splice(1, 0, elem)
+
+// Replace
+months.splice(0, 1, 'January')
